2165d6aef0397d60551b6584d0f980fb9aa4d7b6
LuisF3L1P3dev <dantas.luis@escolar.ifrn.edu.br>
2025-06-04 15:18:13 -0300

    feat: resposta http de teste

--- fastapi_zero/tests/test_app.py
+++ fastapi_zero/tests/test_app.py
@@ -1,11 +1,25 @@
+from http import HTTPStatus
+
 from fastapi.testclient import TestClient
 
 from fastapi_zero.app import app
 
 
 def test_root():
+
+    """
+    Etapa teste triplo (AAA):
+    - A: Arrange - Arranjo
+    - A: Act - Ação
+    - A: Assert - Afirmação
+    """
+
+    # Arrange
     client = TestClient(app)
 
+    # Act
     response = client.get('/')
 
+    # Assert
     assert response.json() == {'hello': 'world'}
+    assert response.status_code == HTTPStatus.OK
