a2f916c5ff00d19fccea19869efe6987281d1a3c
LuisF3L1P3dev <dantas.luis@escolar.ifrn.edu.br>
2025-06-06 12:16:01 -0300

    chore: usando heranca no  UserSchema

--- fastapi_zero/fastapi_zero/schemas.py
+++ fastapi_zero/fastapi_zero/schemas.py
@@ -4,11 +4,9 @@ from pydantic import BaseModel, EmailStr
 class Message(BaseModel):
     message: str
 
-class UserSchema(BaseModel):
-    username: str
-    email: EmailStr
-    password: str
-
 class UserPublic(BaseModel):
     username: str
     email: EmailStr
+
+class UserSchema(UserPublic):
+    password: str
