e7db2dad0597474df015adebf94bff235e9bed9b
LuisF3L1P3dev <dantas.luis@escolar.ifrn.edu.br>
2025-06-08 19:59:29 -0300

    test: add funcao de test_delete_user

--- fastapi_zero/tests/test_app.py
+++ fastapi_zero/tests/test_app.py
@@ -85,3 +85,14 @@ def test_update_user(client):
         'username': 'bob',
         'email': 'bob@example.com',
     }
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'id': 1,
+        'username': 'bob',
+        'email': 'bob@example.com',
+    }
