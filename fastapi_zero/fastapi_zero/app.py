f0c8bcae5e42843368833df5a98f64bc36e1f4cb
LuisF3L1P3dev <dantas.luis@escolar.ifrn.edu.br>
2025-06-08 18:21:54 -0300

    feat: add funcao update_user

--- fastapi_zero/fastapi_zero/app.py
+++ fastapi_zero/fastapi_zero/app.py
@@ -1,6 +1,6 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import HTMLResponse
 
 from fastapi_zero.schemas import (
@@ -9,6 +9,7 @@ from fastapi_zero.schemas import (
     UserDB,
     UserList,
     UserPublic,
+    UserSchema,
 )
 
 app = FastAPI(title='MinhaAPI')
@@ -50,3 +51,20 @@ def create_user(user: UserCreate):
 @app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
 def read_users():
     return {'users': database}
+
+
+@app.put('/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic)
+def update_user(user_id: int, user: UserSchema):
+    if user_id > len(database) or user_id < 1: 
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+
+    user_with_id = UserDB(
+        **user.model_dump(),
+        id=user_id,
+    )
+
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
